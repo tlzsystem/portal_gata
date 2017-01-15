431ef2cc30d52c02b325dd16f3a442f0de4eed6e
Samuel Toloza <tlz.system@gmail.com>
2017-01-15 00:21:50 -0300

    add pagination

--- app/publication/views.py
+++ app/publication/views.py
@@ -2,6 +2,7 @@ from django.views.generic import ListView, DetailView
 from .models import Publication
 
 class ListaPublications(ListView):
+    paginate_by = 10
     queryset = Publication.objects.order_by('-date_published')
 
 class DetailPublication(DetailView):
